2df39196df98335b65bc5f1c91ab6ba541b0f258
aeorxc <aeorxc@gmail.com>
2020-07-10 16:33:37 +0100

    fix dependencies 2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setuptools.setup(
     name="pyieamods",
-    version="1.0.2",
+    version="1.0.3",
     author="aeorxc",
     author_email="author@example.com",
     description="wrapper around IEA Monthly Oil Data Service",
