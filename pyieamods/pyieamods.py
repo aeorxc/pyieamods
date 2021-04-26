06f22dab5d092940907f36add7644eca80ac06fd
ui906879 <gibran.afzal@rwe.com>
2021-04-26 12:57:11 +0100

    feat: add get_series method
    refactor to make code a compact

--- pyieamods/pyieamods.py
+++ pyieamods/pyieamods.py
@@ -1,4 +1,6 @@
-import pandas as pd, os
+import pandas as pd
+import os
+import dask
 import zipfile
 
 names1 = ['commod', 'region', 'balitem', 'date', 'value']
@@ -7,20 +9,29 @@ names3 = ['balitem', 'region', 'commod', 'date', 'value']
 names4 = ['region', 'date', 'value']
 names5 = ['balitem', 'type', 'date', 'value']
 
-# ['CRUDEDAT.TXT', 'NOECDDE.TXT', 'OECDDE.TXT', 'PRODDAT.TXT', 'STOCKDAT.TXT', 'SUMMARY.TXT', 'SUPPLY.TXT']
+filenames = ['CRUDEDAT.TXT', 'NOECDDE.TXT', 'OECDDE.TXT', 'PRODDAT.TXT', 'STOCKDAT.TXT', 'SUMMARY.TXT', 'SUPPLY.TXT']
 
 
-def allmods(sdbstxt_zip_loc, raw=False):
+sdbstxt_mapping = {
+    'PRODDAT.TXT' : { 'prefix' : 'IEA.PROD', 'col_headers' : ['commod', 'region', 'balitem', 'date', 'value'] },
+    'CRUDEDAT.TXT' : { 'prefix' : 'IEA.CRUDE', 'col_headers' : ['commod', 'region', 'balitem', 'date', 'value'] },
+    'OECDDE.TXT' : { 'prefix' : 'IEA.OECDDE', 'col_headers' : ['region', 'commod', 'date', 'value'] },
+    'NOECDDE.TXT' : { 'prefix' : 'IEA.NOECDDE', 'col_headers' : ['region', 'date', 'value'] },
+    'SUMMARY.TXT' : { 'prefix' : 'IEA.SUMMARY', 'col_headers' : ['balitem', 'type', 'date', 'value'] },
+    'STOCKDAT.TXT' : { 'prefix' : 'IEA.STOCKS', 'col_headers' : ['balitem', 'region', 'commod', 'date', 'value'] },
+    'SUPPLY.TXT' : { 'prefix' : 'IEA.SUPPLY', 'col_headers' : ['region', 'commod', 'date', 'value'] },
+}
+
+
+def allmods(sdbstxt_zip_loc:str, raw:bool=False):
     z = zipfile.ZipFile(sdbstxt_zip_loc)
-    res = {}
-    res['PRODDAT'] = proddat(z, raw)
-    res['CRUDEDAT'] = crudedat(z, raw)
-    res['SUPPLY'] = supply(z, raw)
-    res['STOCKSDAT'] = stockdat(z, raw)
-    res['OECDDE'] = oecdde(z, raw)
-    res['NOECDDE'] = noecdde(z, raw)
-    res['SUMMARY'] = summary(z, raw)
+    res = []
+    # filenames = ['PRODDAT.TXT']
+    for filename in filenames:
+        res.append(dask.delayed(read_filename(z, filename, raw)))
 
+    res = dask.compute(*res)
+    res = {x.attrs['filename']:x for x in res}
     return res
 
 
@@ -30,75 +41,36 @@ def to_excel(allmods, outp):
         allmods[dataset].to_excel(outpf)
 
 
-def proddat(z, raw=False):
-    df = pd.read_csv(z.open('PRODDAT.TXT'), sep='\s+', header=None, names=names1, parse_dates=['date'])
+def read_filename(z:str, filename:str, raw:bool=False) -> pd.DataFrame:
+    col_headers = sdbstxt_mapping[filename]['col_headers']
+    prefix = sdbstxt_mapping[filename]['prefix']
+    df = pd.read_csv(z.open(filename), sep='\s+', header=None, names=col_headers, parse_dates=['date'])
+
     if raw:
+        df.attrs['filename'] = filename
         return df
 
-    df['series'] = df.apply(lambda x: 'IEA.PROD.{}.{}.{}'.format(x.commod, x.region, x.balitem), 1)
+    if filename == 'NOECDDE.TXT':
+        df['series'] = df.apply(lambda x: 'IEA.NOECDDE.{}'.format(x.region), 1)
+    elif filename == 'OECDDE.TXT':
+        df['series'] = df.apply(lambda x: 'IEA.OECDDE.{}.{}'.format(x.commod, x.region), 1)
+    elif filename == 'STOCKDAT.TXT':
+        df['series'] = df.apply(lambda x: 'IEA.STOCKS.{}.{}.{}'.format(x.balitem, x.commod, x.region,  ),1)
+    elif filename == 'SUMMARY.TXT':
+        df['series'] = df.apply(lambda x: 'IEA.SUMMARY.{}.{}'.format(x.balitem, x.type), 1)
+    elif filename == 'SUPPLY.TXT':
+        df['series'] = df.apply(lambda x: 'IEA.SUPPLY.{}.{}'.format(x.commod, x.region), 1)
+    else:
+        df['series'] = df.apply(lambda x: '%s.%s.%s.%s' % (prefix, x[col_headers[0]], x[col_headers[1]], x[col_headers[2]]), 1)
     df = df.groupby(['date', 'series']).mean().unstack()['value']
+    df.attrs['filename'] = filename
     return df
 
 
-def crudedat(z, raw=False):
-    df = pd.read_csv(z.open('CRUDEDAT.TXT'), sep='\s+', header=None, names=names1, parse_dates=['date'])
-    if raw:
-        return df
+def get_series(series_name: str, source_loc:str) -> pd.DataFrame:
+    mods = allmods(source_loc)
+    prefixmap = {sdbstxt_mapping[x]['prefix']:x for x in sdbstxt_mapping }
+    prefix = '.'.join(series_name.split('.')[0:2])
 
-    df['series'] = df.apply(lambda x: 'IEA.CRUDE.{}.{}.{}'.format(x.commod, x.region, x.balitem), 1)
-    df = df.groupby(['date', 'series']).mean().unstack()['value']
-    return df
-
-
-def oecdde(z, raw=False):
-    df = pd.read_csv(z.open('OECDDE.TXT'), sep='\s+', header=None, names=names2, parse_dates=['date'])
-    if raw:
-        return df
-
-    df['series'] = df.apply(lambda x: 'IEA.OECDDE.{}.{}'.format(x.commod, x.region), 1)
-    df = df.groupby(['date', 'series']).mean().unstack()['value']
-    return df
-
-
-def noecdde(z, raw=False):
-    df = pd.read_csv(z.open('NOECDDE.TXT'), sep='\s+', header=None, names=names4)
-    if raw:
-        return df
-
-    df['series'] = df.apply(lambda x: 'IEA.NOECDDE.{}'.format(x.region), 1)
-    df = df.groupby(['date', 'series']).mean().unstack()['value']
-    return df
-
-
-def summary(z, raw=False):
-    df = pd.read_csv(z.open('SUMMARY.TXT'), sep='\s+', header=None, names=names5)
-    if raw:
-        return df
-
-    df['series'] = df.apply(lambda x: 'IEA.SUMMARY.{}.{}'.format(x.balitem, x.type), 1)
-    # remove non numbers
-    df = df[df['value'] != 'x']
-    df['value'] = df['value'].astype('float') # turn back to number
-    df = df.groupby(['date', 'series']).mean().unstack()['value']
-    return df
-
-
-def stockdat(z, raw=False):
-    df = pd.read_csv(z.open('STOCKDAT.TXT'), sep='\s+', header=None, names=names3, parse_dates=['date'])
-    if raw:
-        return df
-
-    df['series'] = df.apply(lambda x: 'IEA.STOCKS.{}.{}.{}'.format(x.balitem, x.commod, x.region,  ),1)
-    df = df.groupby(['date', 'series']).mean().unstack()['value']
-    return df
-
-
-def supply(z, raw=False):
-    df = pd.read_csv(z.open('SUPPLY.TXT'), sep='\s+', header=None, names=names2, parse_dates=['date'])
-    if raw:
-        return df
-
-    df['series'] = df.apply(lambda x: 'IEA.SUPPLY.{}.{}'.format(x.commod, x.region), 1)
-    df = df.groupby(['date', 'series']).mean().unstack()['value']
-    return df
-
+    res = mods[prefixmap[prefix]][[series_name]]
+    return res
--- test/test_pyideamods.py
+++ test/test_pyideamods.py
@@ -9,34 +9,40 @@ class TestpyModsIEA(unittest.TestCase):
         dirname, filename = os.path.split(os.path.abspath(__file__))
         test_zip = os.path.join(dirname, 'sdbstxt.zip')
         res = pyieamods.allmods(test_zip)
-        self.assertIn('PRODDAT', res)
-        proddat = res['PRODDAT']
+        self.assertIn('PRODDAT.TXT', res)
+        proddat = res['PRODDAT.TXT']
         self.assertIn('IEA.PROD.CRUDEOIL.AUSTRALI.OMRDEMB', proddat.columns)
 
-        self.assertIn('CRUDEDAT', res)
-        crudedat = res['CRUDEDAT']
+        self.assertIn('CRUDEDAT.TXT', res)
+        crudedat = res['CRUDEDAT.TXT']
         self.assertIn('IEA.CRUDE.AUSTRALI.CRNGFEED.CSNATTERT', crudedat.columns)
 
-        self.assertIn('STOCKSDAT', res)
-        stocksdat = res['STOCKSDAT']
+        self.assertIn('STOCKDAT.TXT', res)
+        stocksdat = res['STOCKDAT.TXT']
         self.assertIn('IEA.STOCKS.INDUSTRY.CRNGFEED.AUSTRIA', stocksdat.columns)
 
-        self.assertIn('SUMMARY', res)
-        summary = res['SUMMARY']
+        self.assertIn('SUMMARY.TXT', res)
+        summary = res['SUMMARY.TXT']
         self.assertIn('IEA.SUMMARY.AMEDEM.FINAL', summary.columns)
 
-        self.assertIn('SUPPLY', res)
-        supply = res['SUPPLY']
+        self.assertIn('SUPPLY.TXT', res)
+        supply = res['SUPPLY.TXT']
         self.assertIn('IEA.SUPPLY.COND.AUSTRALIA', supply.columns)
 
-        self.assertIn('NOECDDE', res)
-        noedcdde = res['NOECDDE']
+        self.assertIn('NOECDDE.TXT', res)
+        noedcdde = res['NOECDDE.TXT']
         self.assertIn('IEA.NOECDDE.BELARUS', noedcdde.columns)
 
-        self.assertIn('OECDDE', res)
-        oecdde = res['OECDDE']
+        self.assertIn('OECDDE.TXT', res)
+        oecdde = res['OECDDE.TXT']
         self.assertIn('IEA.OECDDE.JETANDKERO.AUSTRALI', oecdde.columns)
         self.assertEqual(oecdde['IEA.OECDDE.LPGETHANE.AUSTRALI']['2015-01'].iloc[0], 77.4722)
+
+    def test_get_series_proddat(self):
+        dirname, filename = os.path.split(os.path.abspath(__file__))
+        test_zip = os.path.join(dirname, 'sdbstxt.zip')
+        df = pyieamods.get_series('IEA.PROD.CRUDEOIL.AUSTRALI.OMRDEMB', test_zip)
+        self.assertEqual(df.columns, ['IEA.PROD.CRUDEOIL.AUSTRALI.OMRDEMB'])
 
 
 if __name__ == '__main__':
